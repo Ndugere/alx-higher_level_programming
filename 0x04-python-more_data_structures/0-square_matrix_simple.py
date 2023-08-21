48945114c2ff4d2f40915aaba0a28d9f93c07763
Ndugere <ndungudavid972@gmail.com>
2023-08-21 13:29:18 -0700

    made a few changes after realizing that i was actually making things worse

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,3 +1,5 @@
+#!/usr/bin/python3
+
 def square_matrix_simple(matrix=[]):
     """Compute the square value of all integers of a matrix."""
     return [  # Creates a new list containing the squared rows
