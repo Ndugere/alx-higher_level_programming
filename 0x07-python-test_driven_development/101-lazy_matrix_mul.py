6695b8cc2ae51f6a32ab3cb1e743402ad8c65211
Ndugere <ndungudavid972@gmail.com>
2023-09-08 03:00:01 -0700

    101-lazy_matrix_mul.py

--- 0x07-python-test_driven_development/101-lazy_matrix_mul.py
+++ 0x07-python-test_driven_development/101-lazy_matrix_mul.py
@@ -1,15 +1,24 @@
 #!/usr/bin/python3
-# 101-lazy_matrix_mul.py
-"""Defines a matrix multiplication function using NumPy."""
+"""
+Defines a matrix multiplication function using NumPy.
+"""
 import numpy as np
 
-
 def lazy_matrix_mul(m_a, m_b):
-    """Return the multiplication of two matrices.
+    """
+    Multiply two matrices using NumPy.
 
     Args:
         m_a (list of lists of ints/floats): The first matrix.
         m_b (list of lists of ints/floats): The second matrix.
-    """
 
-    return (np.matmul(m_a, m_b))
+    Returns:
+        A new matrix representing the multiplication of m_a by m_b.
+    """
+    try:
+        result = np.matmul(np.array(m_a), np.array(m_b))
+        return result.tolist()
+    except ValueError:
+        raise ValueError("m_a and m_b can't be multiplied")
+    except TypeError:
+        raise TypeError("invalid data type for matrix multiplication")
