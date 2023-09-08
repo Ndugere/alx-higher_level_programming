530f6c3cea3398539a8237d5ac9fb2fdc2587e14
Ndugere <ndungudavid972@gmail.com>
2023-09-08 03:08:34 -0700

    102-python.c

--- 0x07-python-test_driven_development/102-python.c
+++ 0x07-python-test_driven_development/102-python.c
@@ -1,28 +1,33 @@
-#include <Python.h>
-#include <stdio.h>
-#include <wchar.h>
+/*
+ * File: 102-python.c
+ * Auth: David N Ndungu
+ */
+
+#include "Python.h"
 
 /**
- * print_python_string - Print information about a Python string object
- * @p: Pointer to a Python object
+ * print_python_string - Prints information about Python strings.
+ * @p: A PyObject string object.
  */
-void print_python_string(PyObject *p) {
-    if (PyUnicode_Check(p)) {
-        printf("[.] string object info\n");
-        if (PyUnicode_IS_COMPACT_ASCII(p)) {
-            printf("  type: compact ascii\n");
-        } else if (PyUnicode_IS_COMPACT(p)) {
-            printf("  type: compact unicode object\n");
-        } else {
-            printf("  type: compactified unicode object\n");
-        }
-        Py_ssize_t length = PyUnicode_GET_LENGTH(p);
-        const wchar_t *value = PyUnicode_AsWideCharString(p, &length);
-        printf("  length: %ld\n", length);
-        printf("  value: %ls\n", value);
-        PyMem_Free(value);
-    } else {
-        fprintf(stderr, "[.] string object info\n");
-        fprintf(stderr, "  [ERROR] Invalid String Object\n");
-    }
+void print_python_string(PyObject *p)
+{
+	long int length;
+
+	fflush(stdout);
+
+	printf("[.] string object info\n");
+	if (strcmp(p->ob_type->tp_name, "str") != 0)
+	{
+		printf("  [ERROR] Invalid String Object\n");
+		return;
+	}
+
+	length = ((PyASCIIObject *)(p))->length;
+
+	if (PyUnicode_IS_COMPACT_ASCII(p))
+		printf("  type: compact ascii\n");
+	else
+		printf("  type: compact unicode object\n");
+	printf("  length: %ld\n", length);
+	printf("  value: %ls\n", PyUnicode_AsWideCharString(p, &length));
 }
