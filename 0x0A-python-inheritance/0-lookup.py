a1f56bed9df8a7e4c6d3f7769c75618cc066dbaa
Ndugere <ndungudavid972@com>
2023-10-02 04:22:29 -0700

    0-lookup.py

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,11 +1,8 @@
 #!/usr/bin/python3
-# 1-my_list.py
-"""Defines an inherited list class MyList."""
+# 0-lookup.py
+"""Defines an object attribute lookup function."""
 
 
-class MyList(list):
-    """Implements sorted printing for the built-in list class."""
-
-    def print_sorted(self):
-        """Print a list in sorted ascending order."""
-        print(sorted(self))
+def lookup(obj):
+    """Return a list of an object's available attributes."""
+    return (dir(obj))
